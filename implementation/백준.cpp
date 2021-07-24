6ef6d1c203c0e58aa5684cab4885f0a2099da66c
yelin0323 <48678537+yelin0323@users.noreply.github.com>
2021-07-24 22:29:45 +0900

    Update 백준.cpp

--- implementation/백준.cpp
+++ implementation/백준.cpp
@@ -6,36 +6,11 @@ using namespace std;
 
 int main(void)
 {
-    int n;
-    cin >> n;
+	int a, b, v;
+	cin >> a >> b >> v;
 
-    int word = 0;
-    for (int i = 0; i < n; i++)
-    {
-        string str;
-        cin >> str;
-        int* alp = new int[26]{};
+	cout << (v - b - 1) / (a - b) + 1;
 
-        int prev = -1;
-        int flag = 1;
-        for (int j = 0; j < str.size(); j++)
-        {
-            int temp = str[j] - 'a';
-            if (prev != temp && alp[temp] !=0)
-            {
-                flag = 0;
-                break;
-            }
-            alp[temp]++;
-            prev = temp;
-        }
-        if (flag == 1)
-        {
-            word++;
-        }
-        
-    }
 
-    cout << word;
-    return 0;
+	return 0;
 }
